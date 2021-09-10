f66d763403aee5275e85351846f13b339c8d2684
limenitisNS <hozyans@gmail.com>
2021-09-10 12:11:50 +0700

    added DishForm

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <dish-form />
   </div>
-  <router-view />
 </template>
+
+<script>
+import DishForm from "@/components/IngredientForm/DishForm";
+export default {
+  components: { DishForm },
+};
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/IngredientForm/DishForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-form class="dish-form">
+    <el-form-item>
+      <el-select
+        v-model="selectIngredient"
+        multiple
+        filterable
+        remote
+        :remote-method="handleSearchIngredient"
+      />
+    </el-form-item>
+    <el-form-item>
+      <el-button @click="handleCreateDish">Создать</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class DishForm extends Vue {
+  selectIngredient = [];
+
+  handleSearchIngredient(query: string): void {
+    console.log(query);
+  }
+
+  handleCreateDish(): void {
+    console.log("create");
+    this.selectIngredient = [];
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.dish-form {
+  display: flex;
+}
+</style>
